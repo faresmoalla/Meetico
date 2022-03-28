072ffa586fbd2e456caa10b6a5c1c22a387615e3
faresmoalla <fares.moalla@esprit.tn>
2022-03-28 16:08:42 +0100

    batch

---
+++ Meetico/src/main/java/tn/esprit/meetico/batch/PublicationProcessor.java
@@ -0,0 +1,46 @@
+package tn.esprit.meetico.batch;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.batch.item.ItemProcessor;
+
+import tn.esprit.meetico.entity.Publication;
+import tn.esprit.meetico.entity.Reclamation;
+import tn.esprit.meetico.entity.User;
+import tn.esprit.meetico.repository.PublicationRepository;
+import tn.esprit.meetico.repository.ReclamationRepository;
+import tn.esprit.meetico.repository.UserRepository;
+import tn.esprit.meetico.service.EmailServiceImpl;
+
+public class PublicationProcessor implements ItemProcessor<User, User> {
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	PublicationRepository pubRepo;
+
+	@Autowired
+	EmailServiceImpl emailServiceImpl;
+
+	@Override
+	public User process(User user) throws Exception {
+		
+		//List<Reclamation> r = reclamationRepository.getAllReclamationsClient(user.getUserId());
+//List<Publication> list = pubRepo.findAll();
+		emailServiceImpl.sendEmail(user.getEmail(), "JOIN Test",
+				" Test\r\n" + " Test///////////"
+						);
+		// user.setUser(user);
+		/*for (Publication pub : list ) {
+			
+			if(pub.getContents().contains("@ALL"))
+		}*/
+		
+		return user;
+	
+		
+		
+	}
+
+}
