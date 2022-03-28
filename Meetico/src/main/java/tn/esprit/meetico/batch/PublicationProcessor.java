4d55b47d0ef6412ac8bc2933d5b37a9d3e38ef55
faresmoalla <fares.moalla@esprit.tn>
2022-03-28 18:04:17 +0100

    commit

--- Meetico/src/main/java/tn/esprit/meetico/batch/PublicationProcessor.java
+++ Meetico/src/main/java/tn/esprit/meetico/batch/PublicationProcessor.java
@@ -1,50 +1,50 @@
-package tn.esprit.meetico.batch;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.batch.item.ItemProcessor;
-
-import tn.esprit.meetico.entity.Publication;
-import tn.esprit.meetico.entity.Reclamation;
-import tn.esprit.meetico.entity.User;
-import tn.esprit.meetico.repository.PublicationRepository;
-import tn.esprit.meetico.repository.ReclamationRepository;
-import tn.esprit.meetico.repository.UserRepository;
-import tn.esprit.meetico.service.EmailServiceImpl;
-
-public class PublicationProcessor implements ItemProcessor<User, User> {
-	@Autowired
-	UserRepository userRepository;
-
-	@Autowired
-	PublicationRepository pubRepo;
-
-	@Autowired
-	EmailServiceImpl emailServiceImpl;
-
-	@Override
-	public User process(User user) throws Exception {
-		
-		//List<Reclamation> r = reclamationRepository.getAllReclamationsClient(user.getUserId());
-List<Publication> list = pubRepo.findAll();
-		
-		// user.setUser(user);
-		for (Publication pub : list ) {
-			
-			if(pub.getContents().contains("@all") || pub.getContents().contains("@ALL") ||pub.getContents().contains("@all")  ) {
-				emailServiceImpl.sendEmail(user.getEmail(), "Dear Users",
-						" Une Nouvelle mise a jour \r\n" + ""
-								);
-				
-			}
-				
-		}
-		
-		return user;
-	
-		
-		
-	}
-
-}
+//package tn.esprit.meetico.batch;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.batch.item.ItemProcessor;
+//
+//import tn.esprit.meetico.entity.Publication;
+//import tn.esprit.meetico.entity.Reclamation;
+//import tn.esprit.meetico.entity.User;
+//import tn.esprit.meetico.repository.PublicationRepository;
+//import tn.esprit.meetico.repository.ReclamationRepository;
+//import tn.esprit.meetico.repository.UserRepository;
+//import tn.esprit.meetico.service.EmailServiceImpl;
+//
+//public class PublicationProcessor implements ItemProcessor<User, User> {
+//	@Autowired
+//	UserRepository userRepository;
+//
+//	@Autowired
+//	PublicationRepository pubRepo;
+//
+//	@Autowired
+//	EmailServiceImpl emailServiceImpl;
+//
+//	@Override
+//	public User process(User user) throws Exception {
+//		
+//		//List<Reclamation> r = reclamationRepository.getAllReclamationsClient(user.getUserId());
+//List<Publication> list = pubRepo.findAll();
+//		
+//		// user.setUser(user);
+//		for (Publication pub : list ) {
+//			
+//			if(pub.getContents().contains("@all") || pub.getContents().contains("@ALL") ||pub.getContents().contains("@all")  ) {
+//				emailServiceImpl.sendEmail(user.getEmail(), "Dear Users",
+//						" Une Nouvelle mise a jour \r\n" + ""
+//								);
+//				
+//			}
+//				
+//		}
+//		
+//		return user;
+//	
+//		
+//		
+//	}
+//
+//}
