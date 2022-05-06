4eed4a376e2a7975cc7dd6e0951dd0ff60193e7e
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2022-05-07 00:40:24 +0200

    cc

--- Meetico/Meetico/src/main/java/tn/esprit/meetico/repository/FileDBRepository.java
+++ Meetico/Meetico/src/main/java/tn/esprit/meetico/repository/FileDBRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import tn.esprit.meetico.entity.Comment;
 import tn.esprit.meetico.entity.FileDB;
 
+@Repository
 public interface FileDBRepository extends JpaRepository<FileDB, Long> {
 	
 	
--- Meetico/Meetico/src/main/java/tn/esprit/meetico/repository/FileDBRepositoryTrip.java
+++ Meetico/Meetico/src/main/java/tn/esprit/meetico/repository/FileDBRepositoryTrip.java
@@ -6,11 +6,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import tn.esprit.meetico.entity.FileTrip;
 
 
-
+@Repository
 public interface FileDBRepositoryTrip extends JpaRepository<FileTrip, Long> {
 	
 
