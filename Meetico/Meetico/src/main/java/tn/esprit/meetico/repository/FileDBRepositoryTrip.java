025ae69c598711fd9ac449e4a0409fb7a369201b
ziedlahbib <lahbibmedzied@gmail.com>
2022-05-06 23:43:32 +0100

    Update FileDBRepositoryTrip.java

--- Meetico/Meetico/src/main/java/tn/esprit/meetico/repository/FileDBRepositoryTrip.java
+++ Meetico/Meetico/src/main/java/tn/esprit/meetico/repository/FileDBRepositoryTrip.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import tn.esprit.meetico.entity.FileTrip;
 
 
-@Repository
+
 public interface FileDBRepositoryTrip extends JpaRepository<FileTrip, Long> {
 	
 
