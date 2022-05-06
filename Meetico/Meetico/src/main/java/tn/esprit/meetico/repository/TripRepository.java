3ecc0fa9d7da79a2d120a2df01a435f8b77995db
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2022-05-07 01:30:51 +0200

    correction zied

--- Meetico/Meetico/src/main/java/tn/esprit/meetico/repository/TripRepository.java
+++ Meetico/Meetico/src/main/java/tn/esprit/meetico/repository/TripRepository.java
@@ -4,19 +4,17 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import tn.esprit.meetico.entity.FileTrip;
 import tn.esprit.meetico.entity.Trip;
 import tn.esprit.meetico.entity.User;
 
-
-
+@EnableJpaRepositories
 @Repository
-public interface TripRepository extends JpaRepository<Trip, Integer>{
+public interface TripRepository extends JpaRepository<Trip, Integer> {
 	@Query("Select r FROM User r join r.trips t where t.destination =:destination AND t.startDate =:startdate AND r.city=:city ")
 	List<User> matchingutilisateur(@Param("destination") String destination,@Param("startdate") Date startDate,@Param("city") String city);
 	/*@Modifying
@@ -38,6 +36,4 @@ public interface TripRepository extends JpaRepository<Trip, Integer>{
 	List<User> listdesutilisateurinscritdansvoyage();
 	@Query("Select r FROM Trip r where r.destination=:destination ")
 	List<Trip> searchtrip(@Param("destination") String destination);
-	
-	
 }
