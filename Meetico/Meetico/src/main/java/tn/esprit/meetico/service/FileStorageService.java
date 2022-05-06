80cbbeb2e500e20e7bc53cc9cd309647187593dc
faresmoalla <73170641+faresmoalla@users.noreply.github.com>
2022-05-07 00:03:03 +0200

    cc

--- Meetico/Meetico/src/main/java/tn/esprit/meetico/service/FileStorageService.java
+++ Meetico/Meetico/src/main/java/tn/esprit/meetico/service/FileStorageService.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 import tn.esprit.meetico.entity.FileDB;
-import tn.esprit.meetico.entity.Trip;
+import tn.esprit.meetico.entity.Publication;
+
 import tn.esprit.meetico.repository.FileDBRepository;
-import tn.esprit.meetico.repository.TripRepository;
+import tn.esprit.meetico.repository.PublicationRepository;
+
 
 @Service
 public class FileStorageService {
@@ -18,21 +20,70 @@ public class FileStorageService {
 	@Autowired
 	private FileDBRepository fileDBRepository;
 	
-	@Autowired
-	private TripRepository tripRepo;
-
-	public FileDB store(MultipartFile file) throws IOException {
+//	@Autowired
+//	private TripRepository tripRepo;
+@Autowired
+PublicationRepository  publciationRepo;
+	public FileDB store(MultipartFile file,Long idPublicaiton) throws IOException {
+		
 		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 		FileDB FileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
+		
+		Publication publication = publciationRepo.findById(idPublicaiton).orElse(null);
+		FileDB.setPublication(publication);
+		
 		return fileDBRepository.save(FileDB);
 	}
-
+	
+	public Long ajoutFile(MultipartFile file) throws IOException {
+		
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		FileDB FileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
+		
+		//Publication publication = publciationRepo.findById(idPublicaiton).orElse(null);
+		//FileDB.setPublication(publication);
+		
+		 fileDBRepository.save(FileDB);
+		 return FileDB.getId();
+		
+	}
 	public void deletefile(Long idfile) {
 		fileDBRepository.deleteById(idfile);
 	}
-
-	public FileDB getFile(Long id) {
+	
+	public void affecterImage(Long idFile,Long idPublicaiton) {
+		Publication publication = publciationRepo.findById(idPublicaiton).orElse(null);
+		FileDB filedb = fileDBRepository.findById(idFile).orElse(null);
+		
+		
+		
+		filedb.setPublication(publication);
+		fileDBRepository.save(filedb);
+		
+		
+		
+		
+		}
+	
+public FileDB getFile2(Long id) {
+		
+		//Publication publication = publciationRepo.findById(idPublicaiton).orElse(null);
 		return fileDBRepository.findById(id).orElse(null);
+		
+		
+	}
+
+public FileDB getFile3(Long id) {
+	
+	
+	return fileDBRepository.findById(id).orElse(null);
+	
+	
+}
+	public FileDB getFile(Long idPublicaiton) {
+		
+		Publication publication = publciationRepo.findById(idPublicaiton).orElse(null);
+		return fileDBRepository.GetImageByPub(idPublicaiton);	
 	}
 
 	public Stream<FileDB> getAllFiles() {
