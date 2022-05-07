74ee926191ad355415a09ec55249b19782c50b5d
ziedlahbib <lahbibmedzied@gmail.com>
2022-05-07 04:49:03 +0100

    Revert "correction zied"
    This reverts commit 3ecc0fa9d7da79a2d120a2df01a435f8b77995db.

---
+++ Meetico/Meetico/src/main/java/tn/esprit/meetico/service/FileDBRepositoryTrip.java
@@ -0,0 +1,17 @@
+package tn.esprit.meetico.service;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import tn.esprit.meetico.entity.FileTrip;
+
+
+
+public interface FileDBRepositoryTrip extends JpaRepository<FileTrip, Long> {
+	
+
+}
